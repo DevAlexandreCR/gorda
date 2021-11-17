b0aa06983de3ae20da1508d968d34d86c13032e6
Alexanderbyrush <alexandrecamilor@gmail.com>
2021-11-17 16:11:06 -0500

    feat: run wp chrome

--- index.js
+++ index.js
@@ -1,48 +1,106 @@
-const { app } = require('electron')
-const { loadWhatsApp } = require('./src/window');
-const { createTrayIconFor } = require('./src/tray');
-const { clearServiceWorkers } = require('./src/session');
+// const { app } = require('electron')
+// const { loadWhatsApp } = require('./src/window');
+// const { createTrayIconFor } = require('./src/tray');
+// const { clearServiceWorkers } = require('./src/session');
 const { Client } = require("whatsapp-web.js");
 
 
-let window;
-let tray;
+// let window;
+// let tray;
 
-const isFirstInstance = app.requestSingleInstanceLock();
+// const isFirstInstance = app.requestSingleInstanceLock();
 
-if (!isFirstInstance) {
-  app.quit();
-  return;
-}
+// if (!isFirstInstance) {
+//   app.quit();
+//   return;
+// }
 
-app.on('second-instance', () => {
-  if (window) {
-    if (window.isMinimized()) { window.restore(); }
-    window.focus();
+// app.on('second-instance', () => {
+//   if (window) {
+//     if (window.isMinimized()) { window.restore(); }
+//     window.focus();
+//   }
+// });
+
+// const startApp = () => {
+//   window = loadWhatsApp();
+//   console.log(app.get)
+//   tray = createTrayIconFor(window, app);
+//   const client = new Client({
+//     puppeteer: window,
+// });
+// client.on("message", message => {
+//   if (message.body === "Si a Bueno") {
+//       message.reply('pong');
+//   }
+// });
+// }
+
+
+
+// app.on('ready', startApp);
+// app.on('before-quit', clearServiceWorkers);
+// app.on('window-all-closed', () => app.quit());
+
+
+const puppeteer = require("puppeteer");
+
+  async function main() {
+    const browser = await puppeteer.launch({
+      defaultViewport: {
+        width: 720,
+        height: 1280
+      },
+      headless: false,
+    });
+    
+  const SESSION_FILE_PATH = "./session.json";
+  const fs = require("fs");
+
+
+  let sessionData;
+  if (fs.existsSync(SESSION_FILE_PATH)) {
+      sessionData = require(SESSION_FILE_PATH);
   }
-});
 
-const startApp = () => {
-  window = loadWhatsApp();
-  tray = createTrayIconFor(window, app);
+  // const page = await browser.newPage();
   const client = new Client({
-    puppeteer: window,
+    session: sessionData,
+    puppeteer: {
+      browserWSEndpoint: browser.wsEndpoint()
+    }
+  });
+  
+  client.on("authenticated", (session) => {
+    sessionData = session;
+    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+
 });
-client.on("message", message => {
-  if (message.body === "Si a Bueno") {
-      message.reply('pong');
+
+
+    client.on('message', msg => {
+      if (msg.body == 'hola') {
+          msg.reply('holo');
+      }
+  });
+
+    client.initialize();
+
+   
+    // await page.goto('https://web.whatsapp.com/');
   }
-});
-}
+
+  main();
+  
 
 
-
-app.on('ready', startApp);
-app.on('before-quit', clearServiceWorkers);
-app.on('window-all-closed', () => app.quit());
-
-
+    
 
   
+
 
 
