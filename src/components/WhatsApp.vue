5754071e6fb7eb297b5755c7735000560915dbcb
Alexanderbyrush <alexandrecamilor@gmail.com>
2021-11-18 11:31:00 -0500

    feat: add button to init app

---
+++ src/components/WhatsApp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="WhatsApp">
+    <button type="button" class="btn btn-primary btn-lg">Iniciar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WhatsApp',
+}
+</script>
+
+<style scoped>
+
+.WhatsApp {
+  margin-top: 30px;
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+      <img src="../assets/mess.png" alt="" height="100" width="100">
+  <WhatsApp/>
+  </div>
+  
+
+</template>
+
+<script>
+// @ is an alias to /src
+import WhatsApp from '@/components/WhatsApp.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    WhatsApp
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
